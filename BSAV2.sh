0a36c9305fe9a4dff6764b3b7c5383cae18a9e03
Everson4t <everson4t@gmail.com>
2021-02-13 09:14:59 -0300

    ver 0.2

--- BSAV2.sh
+++ BSAV2.sh
@@ -2,7 +2,7 @@
 
 pip3 install oci pyclamd
 
-echo "ClamAV Install Started"
+echo "Install and Update ClamAV Started"
 yum -y install clamav clamav-scanner-systemd
 ln -s /etc/clamd.d/scan.conf /etc/clamd.conf
 echo "LocalSocket /run/clamd.scan/clamd.sock" >>/etc/clamd.conf
@@ -10,9 +10,4 @@ setsebool -P antivirus_can_scan_system 1
 freshclam
 systemctl start clamd@scan
 systemctl enable clamd@scan
-echo "ClamAV Install Ended"
-
-echo "Scan objects"
-wget -P /root https://raw.githubusercontent.com/Everson4t/antivirus-for-objectstore/main/scan_bucket.py
-chmod 744 /root/scan_bucket.py
-/usr/bin/python3 /root/scan_bucket.py checkinobj quarantine >> /root/report.txt
+echo "ClamAV Installation Ended"
