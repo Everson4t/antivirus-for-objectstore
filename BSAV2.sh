2090cd8e657b76547c88f5d67bdce5ba2710956e
Everson4t <everson4t@gmail.com>
2021-02-13 15:29:39 -0300

    ver 0.16

--- BSAV2.sh
+++ BSAV2.sh
@@ -3,7 +3,7 @@
 pip3 install oci pyclamd
 #
 echo "Install and Update ClamAV Started"
-yum -y install clamav clamd
+yum -y install clamav clamd 
 ln -s /etc/clamd.d/scan.conf /etc/clamd.conf
 echo "LocalSocket /run/clamd.scan/clamd.sock" >>/etc/clamd.conf
 /usr/sbin/setsebool -P antivirus_can_scan_system 1 
