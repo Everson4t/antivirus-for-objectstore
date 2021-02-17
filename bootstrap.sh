6b5e832eee1c9ae5175ff2a906ab6d69661dc7e7
Everson4t <everson4t@gmail.com>
2021-02-17 14:33:26 -0300

    Release 0.31

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,7 @@ echo "Install and Update ClamAV Started"
 yum -y install clamav clamd 
 ln -s /etc/clamd.d/scan.conf /etc/clamd.conf
 echo "LocalSocket /run/clamd.scan/clamd.sock" >>/etc/clamd.conf
+echo "StreamMaxLength 1000M" >>/etc/clamd.conf
 /usr/sbin/setsebool -P antivirus_can_scan_system 1 
 /usr/bin/freshclam 
 /usr/bin/systemctl start clamd@scan 
