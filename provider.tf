14d3a3ba9d9fe19524fc4db9af5025567e204f23
Everson4t <everson4t@gmail.com>
2021-04-15 19:55:50 -0300

    v0.95

--- provider.tf
+++ provider.tf
@@ -3,9 +3,9 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  region               = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  region           = var.region
+  fingerprint      = var.fingerprint
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
 }
